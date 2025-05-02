f7a74627f3d5b28f38bd99e78a57540d5e3fafd9
Luis Fernando R. Honorato <luisferando.honorato@gmail.com>
2025-05-01 23:09:01 -0300

    Refactor code structure for improved readability and maintainability

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("menu-toggle").addEventListener("click", () => {
+    const mobileMenu = document.getElementById("mobile-menu");
+    mobileMenu.classList.toggle("open");
+  });
+  
